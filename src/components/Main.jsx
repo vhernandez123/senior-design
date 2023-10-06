e1e3b50e9d4a95ce6bf6d5f8460b68650d44337c
Valeria Hernandez <vhernandez@oakland.edu>
2023-10-06 17:00:25 -0400

    lin change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,32 @@
-import React from 'react'
-import videoBG from '../assets/videoBG.mp4'
-import { useAuth0 } from '@auth0/auth0-react'; // Import useAuth0
-import { Auth0Provider } from'@auth0/auth0-react';
+import React from "react";
+import videoBG from "../assets/videoBG.mp4";
+import { useAuth0 } from "@auth0/auth0-react"; // Import useAuth0
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-
-
 const Main = () => {
-    const { loginWithRedirect } = useAuth0(); // Get Auth0 hook
-  
-    const handleLogin = () => {
-        loginWithRedirect(
-           // Redirect to the landi);
-        );
-    return (
-        <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-      >
-        <div className='main'>
-          <div className="overlay"></div>
-          <video src={videoBG} autoPlay loop muted />
-          <div className="content">
-            <button className="centered-button" onClick={handleLogin}>
-              Login
-            </button>
-          </div>
+  const { loginWithRedirect } = useAuth0(); // Get Auth0 hook
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <div className="main">
+        <div className="overlay"></div>
+        <video src={videoBG} autoPlay loop muted />
+        <div className="content">
+          <button className="centered-button" onClick={handleLogin}>
+            Login
+          </button>
         </div>
-        </Auth0Provider>
-      );
-    };
-
-export default Main
+      </div>
+    </Auth0Provider>
+  );
+};
+export default Main;
