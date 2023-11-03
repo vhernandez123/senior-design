5db1e823fd53d6e5891cc2148ec682137137c031
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-11-03 17:53:52 -0400

    Brought daily log form to current master branch

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import Main from "./components/Main.jsx"; // Import your Main component
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home.js";
 import AddPet from "./pages/AddPet";
+import UpdateFood from "./pages/UpdateFood.js";
+import UpdateBathroom from "./pages/UpdateBathroom.js";
+import UpdateBehavior from "./pages/UpdateBehavior.js";
+import UpdateOther from "./pages/UpdateOther.js";
+
 import "normalize.css";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -24,6 +29,17 @@ ReactDOM.render(
           <Route path="/app" element={<App />} />
           <Route path="/home" element={<Home />} /> {/* Add this route */}
           <Route path="/AddPet" element={<AddPet />} />
+          <Route path="UpdateForm" element={
+           <div>
+            <UpdateFood/>
+            <hr/>
+            <UpdateBathroom/>
+            <hr/>
+            <UpdateBehavior/>
+            <hr/>
+            <UpdateOther/>
+           </div> 
+          }/>
         </Routes>
       </Router>
     </Auth0Provider>
