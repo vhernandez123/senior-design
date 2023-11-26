1bd2262fe20d7d50383f4f17d8b671a01cc98ab9
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-11-25 22:03:53 -0500

    Added location consent popup

--- src/pages/VetFinder.js
+++ src/pages/VetFinder.js
@@ -1,14 +1,71 @@
 import * as React from "react";
 import Map from "../components/Map";
+import { useState} from "react";
 
-const VetFinder5 =() => {
+
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	RadioGroup,
+    Radio,
+    FormControlLabel
+  } from "@mui/material";
+
+
+const VetFinder =() => {
+    const [open, setOpen] = React.useState(true);
+    const handleOpen = () => setOpen(true);
+    const [showMap, setShowMap] = useState(false);
+    const [agree, setAgree] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+        //handle clicking button to close dialog
+        if (agree){
+            setShowMap(true);
+            } else {
+                setShowMap(false);
+            }
+    }
     return (
         <div>
             <br/><br/>
             <h2>Veterinarians Near You</h2>
             <br/><br/>
-            <Map></Map>
+            <Dialog open={open} onClose={handleClose}>
+					<DialogTitle>Find Vets Near You</DialogTitle>
+					<DialogContent>
+					<DialogContentText>
+						To discover veterenarians in your area, please agree to let access your browser location. 
+					</DialogContentText>
+					<br/>
+                    <RadioGroup
+                        aria-labelledby="location-consent"
+                        value={agree}
+                        name="location-consent"
+                        onChange={(e) => setAgree(e.target.value)}
+                    >
+                        <FormControlLabel value={true} control={<Radio />} label="Agree" />
+                        <FormControlLabel value={false} control={<Radio />} label="Do not use my browser location" />
+                    </RadioGroup>
+					</DialogContent>
+					<DialogActions>
+					<Button onClick={() => handleClose()}>Submit</Button>
+					</DialogActions>
+				</Dialog>	
+                <br/><br/>
+                { //added
+					showMap ? 
+					<Map></Map> 
+					: <div>
+                        <h3>Allow browser location access to see the map</h3>
+                        <Button onClick={() => handleOpen()}>Grant Access</Button>
+                        </div>}
         </div>
     )
 }
-export default VetFinder5;
+export default VetFinder;
