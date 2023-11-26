3ef3b3d49c8e656101678d6e0f505146d12b7a15
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-11-25 21:45:06 -0500

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,12 +3,12 @@ import * as React from "react";
 const Map = () => {
 const sourceURL = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBDUcFZdWRZ5SQOq_q4OYE3DoDhKMRcxgk&q=vet+near+me";
   return (
-    <div >
+    <div width="640px" height="640px">
     <iframe
 				src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBDUcFZdWRZ5SQOq_q4OYE3DoDhKMRcxgk&q=vet+near+me"
-				width="95%"
+				width="640px"
 				//sandbox="aasda"
-				height="640vh"
+				height="340px"
 				id=""
 				className=""
 				sandbox="allow-scripts allow-same-origin"
