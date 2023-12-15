c1f1c0b5e943bd8804f03617a4bd38379be5551f
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-12-15 00:07:11 -0500

    deleted unworking image optimization

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,6 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "../App.css";
 import CatFoot from "../images/cat.jpg";
 // import useUserFinder  from "../components/userFinder.js";
-import { useWindowWidth } from '@react-hook/window-size';
-import imageCompression from 'browser-image-compression';
 
 const DataTable = lazy(() => import('./HomeTable.js'));
 
@@ -20,7 +18,7 @@ function Home() {
   const [userId, setUserId] = useState(null);
   // const { user, getIdTokenClaims } = useAuth0();
 
-  const [imgFile, setImgFile] = useState(CatFoot);
+  const [imgFile, setImgFile] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -58,28 +56,6 @@ function Home() {
     fetchUserId();
   }, [getIdTokenClaims, user]); 
 
-
-  useEffect(() => {
-    const optImage = async () => {
-    
-      const options = {
-        maxSizeMB: 1,
-        maxWidthOrHeight: useWindowWidth
-      }
-      try {
-        const compressedFile = await imageCompression(imgFile, options);
-        console.log(compressedFile.size/1024/1024);
-        setImgFile(compressedFile);
-      } catch (error) {
-        console.log("Error compressing image: ", error);
-      }
-    
-    }
-  }
-  );
-  
-
-
   return (
     <div className="app">
       <Navbar />
@@ -90,7 +66,7 @@ function Home() {
           </Suspense> 
         </div>
         <div class="image-container">
-          <img class="bottom-image"src={imgFile} alt="cat1" className="catimage" />
+          <img class="bottom-image"src={CatFoot} alt="cat1" className="catimage" />
         </div>
       </div>
       <Footer />
