3ec5d452dfc45702ea50a04e0df99fd9df44b1e4
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-12-17 21:10:15 -0500

    added react-snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {hydrate, render} from "react-dom";
 import "./index.css";
 import App from "./App";
 import Main from "./components/Main.jsx";
@@ -22,6 +23,87 @@ import EditPet from "./pages/EditPet.js";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  console.log("Using hydrate");
+  hydrate(
+    <React.StrictMode>
+    <Auth0Bugs>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/app" element={<App />} />
+          <Route path="/home/" element={<Home />} />
+          <Route path="/AddPet" element={<AddPet />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Checklist" element={<Checklist />} />
+          <Route path="/VetFinder" element={<VetFinder />} />
+          <Route
+            path="/UpdateBehavior/:logsID/:petID"
+            element={<UpdateBehavior />}
+          />
+          <Route
+            path="/LogMedication/:logsID/:petID"
+            element={<LogMedication />}
+          />
+          <Route path="/pet/:petID" element={<PetDetails />} />
+          <Route
+            path="/LoggingForms/:logsID/:petID"
+            element={<LoggingForms />}
+          />
+          <Route path="/log-pet/:petID" element={<LogPet />} />
+          <Route path="/log-food/:logsID/:petID" element={<LogFood />} />
+          <Route
+            path="/UpdateBathroom/:logsID/:petID"
+            element={<UpdateBathroom />}
+          />
+          <Route path="/EditPet/:petID" element={<EditPet />} />
+        </Routes>
+      </Router>
+    </Auth0Bugs>
+  </React.StrictMode>, rootElement);
+} else {
+  console.log("Using render");
+  render(
+    <React.StrictMode>
+    <Auth0Bugs>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/app" element={<App />} />
+          <Route path="/home/" element={<Home />} />
+          <Route path="/AddPet" element={<AddPet />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Checklist" element={<Checklist />} />
+          <Route path="/VetFinder" element={<VetFinder />} />
+          <Route
+            path="/UpdateBehavior/:logsID/:petID"
+            element={<UpdateBehavior />}
+          />
+          <Route
+            path="/LogMedication/:logsID/:petID"
+            element={<LogMedication />}
+          />
+          <Route path="/pet/:petID" element={<PetDetails />} />
+          <Route
+            path="/LoggingForms/:logsID/:petID"
+            element={<LoggingForms />}
+          />
+          <Route path="/log-pet/:petID" element={<LogPet />} />
+          <Route path="/log-food/:logsID/:petID" element={<LogFood />} />
+          <Route
+            path="/UpdateBathroom/:logsID/:petID"
+            element={<UpdateBathroom />}
+          />
+          <Route path="/EditPet/:petID" element={<EditPet />} />
+        </Routes>
+      </Router>
+    </Auth0Bugs>
+  </React.StrictMode>, rootElement);
+}
+
+/*
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Bugs>
@@ -60,3 +142,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+*/
